8d2a8d0bcf7b4a17e3b93a812e887b2a30bf39e2
abdullahgr <aliabdullah292631@gmail.com>
2024-11-04 20:29:29 +0500

    Naive implementation is complete.

--- Module 2/Greatest Common Divisor/naive.py
+++ Module 2/Greatest Common Divisor/naive.py
@@ -1,5 +1,14 @@
 def get_greatest_common_divisor(first, second):
-    return first
+    # Find minimum of a and b
+    result = min(a, b)
+
+    while result:
+        if a % result == 0 and b % result == 0:
+            break
+        result -= 1
+
+    # Return the gcd of a and b
+    return result
 
 if __name__ == '__main__':
     a, b = map(int, input().split())
